c1eb47b6e6bd489eb7be4edd9db3c32dfd1cee71
Michael Epelboim <sdrmike@outlook.com>
2022-11-16 17:59:01 +0800

    flats index and flats show complete

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,19 @@
+require 'open-uri'
+
 class FlatsController < ApplicationController
+  before_action :set_flats
+
+  def home; end
+
+  def show
+    id = params[:id]
+    @flat = @flats.select { |flat| flat['id'] == id.to_i }.first
+  end
+
+  private
+
+  def set_flats
+    url = 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json'
+    @flats = JSON.parse(URI.open(url).read)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'flats#home'
+  get 'flats/:id', to: 'flats#show', as: :flat
 end
